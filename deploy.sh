4c99b20b5bb61cfd28979393ce194234c6fd0e1b
qiwen98 <45889437+qiwen98@users.noreply.github.com>
2022-06-16 10:18:01 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,8 @@ npm run deploy
 git checkout gh-pages
 $ git checkout master -- models
 $ git add models
+$ git add node_modules/.cache/
 $ git commit -m "Adding 'models' directory from 'master' branch."
+$ git push --all
+
+$ git checkout master
