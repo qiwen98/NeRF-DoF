b717a773f0c86ccbbb40632304d59eff7d83dde5
qiwen98 <45889437+qiwen98@users.noreply.github.com>
2022-09-12 18:59:51 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,25 @@ const outDir= resolve(__dirname, 'dist')
 
 export default defineConfig({
   root,
-  base: '/Stegano_M_Vis/',
+  base: '/DSteganoM/',
   build: {
     outDir,
     emptyOutDir:true,
     rollupOptions: {
       input: {
         main: resolve(root, 'index.html'),
-        nested: resolve(root, 'page_one', 'index.html')
+        page1: resolve(root, 'oursCMUGatedCover', 'index.html'),
+        page2: resolve(root, 'oursCMUGatedSecret', 'index.html'),
+        page3: resolve(root, 'oursCMUGatedCorrupted', 'index.html'),
+        page4: resolve(root, 'skipCMUGatedCover', 'index.html'),
+        page5: resolve(root, 'skipCMUGatedSecret', 'index.html'),
+        page6: resolve(root, 'skipCMUGatedCorrupted', 'index.html'),
+        page7: resolve(root, 'oursMTMGatedCover', 'index.html'),
+        page8: resolve(root, 'oursMTMGatedSecret', 'index.html'),
+        page9: resolve(root, 'oursMTMGatedCorrupted', 'index.html'),
+        page10: resolve(root, 'skipMTMGatedCover', 'index.html'),
+        page11: resolve(root, 'skipMTMGatedSecret', 'index.html'),
+        page12: resolve(root, 'skipMTMGatedCorrupted', 'index.html'),
       }
     }
   }
