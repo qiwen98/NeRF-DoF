814032ab46f92eca33c8c4ef38896a155c44b090
qiwen98 <45889437+qiwen98@users.noreply.github.com>
2022-12-19 14:19:44 +0800

    second-commit

--- vite.config.js
+++ vite.config.js
@@ -7,28 +7,13 @@ const outDir= resolve(__dirname, 'dist')
 
 export default defineConfig({
   root,
-  base: '/DSteganoM/',
+  base: '/NeRF-DoF/',
   build: {
     outDir,
     emptyOutDir:true,
     rollupOptions: {
       input: {
         main: resolve(root, 'index.html'),
-        page1: resolve(root, 'oursCMUGatedCover', 'index.html'),
-        page2: resolve(root, 'oursCMUGatedSecret', 'index.html'),
-        page3: resolve(root, 'oursCMUGatedCorrupted', 'index.html'),
-        page4: resolve(root, 'skipCMUGatedCover', 'index.html'),
-        page5: resolve(root, 'skipCMUGatedSecret', 'index.html'),
-        page6: resolve(root, 'skipCMUGatedCorrupted', 'index.html'),
-        page7: resolve(root, 'oursMTMGatedCover', 'index.html'),
-        page8: resolve(root, 'oursMTMGatedSecret', 'index.html'),
-        page9: resolve(root, 'oursMTMGatedCorrupted', 'index.html'),
-        page10: resolve(root, 'skipMTMGatedCover', 'index.html'),
-        page11: resolve(root, 'skipMTMGatedSecret', 'index.html'),
-        page12: resolve(root, 'skipMTMGatedCorrupted', 'index.html'),
-        page13: resolve(root, 'oursMixGatedCover', 'index.html'),
-        page14: resolve(root, 'oursMixGatedSecret', 'index.html'),
-        page15: resolve(root, 'oursMixGatedCorrupted', 'index.html'),
       }
     }
   }
